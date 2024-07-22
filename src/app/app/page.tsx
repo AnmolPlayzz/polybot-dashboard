2f613108e7b8f00ec5842ab46d1280a79dca8f4c
AnmolPlayzz <74979911+AnmolPlayzz@users.noreply.github.com>
2024-07-22 13:39:31 +0530

    v0.1.3 - fix

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,5 +1,7 @@
 import {redirect} from "next/navigation";
 import {verifyAuth, verifyToken} from "@/lib/auth/auth";
+import styles from "./page.module.css"
+import Image from "next/image";
 export default async function DashboardHome() {
     const result = await verifyAuth()
 
@@ -15,6 +17,13 @@ export default async function DashboardHome() {
     }
 
     return <>
-        <p>Dashboard</p>
+        <main className={styles.dashboardMain}>
+            <div className={styles.tempDetails}>
+                <p>{discordUser.id}</p>
+                <Image src={discordUser.avatar_url || "https://cdn.discordapp.com/avatars/1111870769027371094/d678d4e7b9eefc9206b1be8b6dac5a34.webp?size=4096"} alt={"user avatar"} width={200} height={200}/>
+                <p>{discordUser.global_name}</p>
+                <p>{discordUser.username}</p>
+            </div>
+        </main>
     </>
 }
