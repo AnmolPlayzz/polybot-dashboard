5ef6fac3444a59d758545286c0c94f6b2ce85b82
AnmolPlayzz <74979911+AnmolPlayzz@users.noreply.github.com>
2024-07-22 12:29:20 +0530

    v0.1.2 - Redirect URL fix

--- src/lib/auth/discord.ts
+++ src/lib/auth/discord.ts
@@ -2,5 +2,5 @@ import { Discord } from "arctic";
 
 const clientId:string= process.env.CLIENT_ID || "";
 const clientSecret:string = process.env.CLIENT_SECRET || "";
-
-export const discord: Discord = new Discord(clientId, clientSecret, "http://localhost:3000/api/auth/callback");
+const redirectUrl:string = process.env.REDIRECT_URL || "http://localhost:3000";
+export const discord: Discord = new Discord(clientId, clientSecret, `${redirectUrl}/api/auth/callback`);
